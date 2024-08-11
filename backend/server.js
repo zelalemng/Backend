d24a02385ebc6c2b0c37a3b2f711d361a05a406b
zelalemng <122828485+zelalemng@users.noreply.github.com>
2024-08-11 13:12:10 -0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const { errorHandler } = require("./middleware/error");
 const connectDB = require("./config/database");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
-
+console.log(process.env.NODE_ENV)
 connectDB();
 
 const app = express();
@@ -31,17 +31,17 @@ app.use("/api/restaurants", require("./routes/restaurants"));
 app.use("/api/users", require("./routes/users"));
 app.use('/api/orders', require('./routes/orderRoutes'));
 app.use('/api/categories', require('./routes/categories'));
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
-  app.get("*", (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, "../", "frontend", "build", "index.html")
-    )
-  );
-} else {
-  app.get("/", (req, res) => res.send("Please set to production"));
-}
+app.use('/', express.static(path.join(__dirname, "public")));
+//if (process.env.NODE_ENV === "production") {
+  
+  //app.get("*", (req, res) =>
+    //res.sendFile(
+      //path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    //)
+  //);
+//} else {
+  //app.get("/", (req, res) => res.send("Please set to production"));
+//}
 
 app.use(errorHandler);
 
