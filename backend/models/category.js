5688d3955ea52e8b889f9571e3b670614a34a7d5
zelalemng <122828485+zelalemng@users.noreply.github.com>
2024-08-12 13:13:39 -0700

    Update category.js

--- backend/models/category.js
+++ backend/models/category.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const CategorySchema = new mongoose.Schema({
-  category_id: { type: String, required: true },
+  
   category_Name: { type: String, required: true },
   category_description: { type: String, required: true },
   category_status: { type: String, enum: ['active', 'inactive'], required: true },
