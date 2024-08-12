37b33ffdde2a823a6cfaafeede73853b6856ff8f
zelalemng <122828485+zelalemng@users.noreply.github.com>
2024-08-12 12:47:36 -0700

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,32 +1,8 @@
 const jwt = require("jsonwebtoken");
 const asyncHandler = require("express-async-handler");
 const User = require("../models/user");
-
-const fetchuser =(req, res, next)=>{
-    // to convert auth token into user details
-    const token = req.header('auth-token');
-    console.log(token);
-    if(!token){
-        console.log("if")
-        return res.status(401).send({error:"Please authenticate using adfs valid token"})
-    }
-    try{
-        console.log("try")
-        console.log(token);
-        const data = jwt.verify(token, process.env.JWT_SECRET);
-        req.user= data.user;
-        next()
-    }catch(error){
-        console.log("catch")
-       return res.status(401).send({error:"Please authenticate using a valid token"})
-    }
-}
-
-module.exports = {
-    fetchuser
-};
 /**
- * Protect Route Middleware - Helper to protect routes based on login status
+ * Protect Route Middleware - Helper to protect routes based on login status*/
  const protect = asyncHandler(async (req, res, next) => {
   let token;
 
@@ -55,4 +31,4 @@ module.exports = {
 module.exports = {
   protect,
 };
-**/
+
