a7c0cfcfb4a60f95dd80cb1a5791a83ed79203c7
zelalemng <122828485+zelalemng@users.noreply.github.com>
2024-10-24 17:57:44 +0300

    Update Order.js

--- backend/models/Order.js
+++ backend/models/Order.js
@@ -1,3 +1,4 @@
+/**
 const mongoose = require('mongoose');
 
 const OrderSchema = new mongoose.Schema({
@@ -11,3 +12,22 @@ const OrderSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model('Order', OrderSchema);
+**/
+const mongoose = require('mongoose');
+
+const OrderSchema = new mongoose.Schema({
+  order_id: { type: String, required: true, unique: true },
+  name: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  category_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true },
+  category_type: { type: String },
+  serviceName: { type: mongoose.Schema.Types.ObjectId, ref: 'Service', required: true },
+  order_description: { type: String, required: true },
+  order_status: { type: String, enum: ['pending', 'started', 'completed', 'rejected'], required: true },
+  order_price: { type: Number, required: true },
+  
+  order_createdAt: { type: Date, default: Date.now },
+  order_completedAt: { type: Date },
+});
+
+module.exports = mongoose.model('Order', OrderSchema);
+
