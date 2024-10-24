c3087f40387046ffa9beab853a37dc2fe924848a
zelalemng <122828485+zelalemng@users.noreply.github.com>
2024-10-25 01:49:14 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -39,10 +39,12 @@ app.use('/api/categories', require('./routes/categories'));
 app.use('/', express.static(path.join(__dirname, "public")));
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/build")));
+	app.use(express.static(path.join(__dirname, "/frontend/public")));
 
 	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+		if (req.method === 'GET') {
+			res.sendFile(path.join(__dirname, 'public', 'index.html'));
+		};
 	});
 }
 //if (process.env.NODE_ENV === "production") {
