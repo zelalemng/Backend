471df6b38036111e0c569056a87288a4e3409d6b
zelalemng <122828485+zelalemng@users.noreply.github.com>
2024-10-24 18:53:50 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,16 @@ const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/error");
 const connectDB = require("./config/database");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const port = process.env.PORT || 5000;
 console.log(process.env.NODE_ENV)
+const path = require("path");
 connectDB();
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors({
     origin: ["https://agentpro.onrender.com"],
@@ -26,16 +29,14 @@ app.use('/api/services', require('./routes/serviceRoutes'));
 app.use("/api/users", require("./routes/users"));
 app.use('/api/orders', require('./routes/orderRoutes'));
 app.use('/api/categories', require('./routes/categories'));
-//app.use('/', express.static(path.join(__dirname, "public")));
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.use('/', express.static(path.join(__dirname, "public")));
 
-  // Handle React routing, return all requests to React app
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"));
-  });
-} else {
-  app.get("/", (req, res) => res.send("Please set to production"));
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+	});
 }
 //if (process.env.NODE_ENV === "production") {
   
