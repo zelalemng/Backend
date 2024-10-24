9bfb69499190e664a880f16fe198db4c97c11a79
zelalemng <122828485+zelalemng@users.noreply.github.com>
2024-10-25 01:10:56 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,11 @@ app.use(express.urlencoded({ extended: false }));
     //methods: ["GET", "POST", "PUT", "DELETE"],
     //credentials: true
 //}));
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 
 app.use("/api/tables", require("./routes/tables"));
